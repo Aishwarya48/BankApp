59ffa84a42f02ffbd61c72d353572014d3e2effe
Aishwarya <68149940+Aishwarya48@users.noreply.github.com>
2021-04-14 22:11:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title>Home Page</title>
 	<link  rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
-	<link rel="stylesheet" type="text/css" href="../BankAppmaster/Css/style.css">
+	<link rel="stylesheet" type="text/css" href="../BankApp/Css/style.css">
 
 </head>
 <body>
